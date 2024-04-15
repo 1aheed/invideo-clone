640a37d3519c70c55175348ce1b010162d95693b
Aheed Sajid <aheedsajid@users.noreply.huggingface.co>
2024-04-15 09:27:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def get_pexels_video(keyword):
         "per_page": 20,  # Increase the per_page parameter to get more results
         "orientation": "landscape", 
         "size": "large",
-        "min_width": 1920,
-        "min_height": 1080
+        "min_width": 2020,
+        "min_height": 1180
     }
     response = requests.get("https://api.pexels.com/videos/search", headers=headers, params=params)
 
