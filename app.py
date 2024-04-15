89f8552dfe060174bee87d1fb2b2fb1d23e718ab
Aheed Sajid <aheedsajid@users.noreply.huggingface.co>
2024-04-15 05:43:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def process_video(topic):
     for scene in scene_info:
         video_clip = VideoFileClip(scene['video_filename']).subclip(0, scene['voiceover_duration'])
         video_clip = video_clip.set_audio(AudioFileClip(scene['voiceover_filename']))
-        video_clip = video_clip.resize((1920, 1080)) 
+        video_clip = video_clip.resize((1920, 1080), resample='lanczos') 
         scene_videos.append(video_clip)
 
     final_video = concatenate_videoclips(scene_videos)
